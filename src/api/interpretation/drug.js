51362be907f5c3c6c43501fddde4354b03867b41
ZhengCQ <zhengchenqing@aliyun.com>
2019-04-14 22:12:13 +0800

    增加解读管理-药物和个性特征

--- src/api/interpretation/drug.js
+++ src/api/interpretation/drug.js
@@ -7,3 +7,27 @@ export function glistDrug(query) {
     params: query
   })
 }
+
+export function gallPrimary(name) {
+  return request({
+    url: '/disease/all_primary',
+    method: 'get',
+    params: { name }
+  })
+}
+
+export function gallSecondary(primary_name) {
+  return request({
+    url: '/disease/all_secondary',
+    method: 'get',
+    params: { primary_name }
+  })
+}
+
+export function gallDrug(secondary_name) {
+  return request({
+    url: '/drug/all_drug',
+    method: 'get',
+    params: { secondary_name }
+  })
+}
--- src/api/interpretation/personal.js
+++ src/api/interpretation/personal.js
@@ -15,3 +15,27 @@ export function gdeleteIndicate(query) {
     params: query
   })
 }
+
+export function gallPrimary(name) {
+  return request({
+    url: '/disease/all_primary',
+    method: 'get',
+    params: { name }
+  })
+}
+
+export function gallSecondary(primary_name) {
+  return request({
+    url: '/disease/all_secondary',
+    method: 'get',
+    params: { primary_name }
+  })
+}
+
+export function gallPersonality(secondary_name) {
+  return request({
+    url: '/personal/all_personality',
+    method: 'get',
+    params: { secondary_name }
+  })
+}
